032c8a183f1d9e797237c4289486fb4e86b66348
anna <annacarolinagarrido@gmail.com>
2022-11-16 15:46:49 -0300

    Initial commit

---
+++ projetogerminare/src/Pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { MainDiv } from './style';
+export default function HomePage() {
+    return (
+        <>
+            <MainDiv>
+                <div className="slider"> </div>
+                <div className="generalInfo"> </div>
+            </MainDiv>
+        </>
+    )
+}
---
+++ projetogerminare/src/Pages/Home/style.jsx
@@ -0,0 +1,12 @@
+import { styled } from "@mui/material";
+import Box from "@mui/material"
+export const MainDiv = styled('div')({
+    '& .slider' : {
+        backgroundColor: '#D2D2D2',
+        height: '603px'
+    },
+    '& .generalInfo' : {
+        backgroundColor: '#A8A8A8',
+        height: '247px'
+    }
+})
