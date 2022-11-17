c05f265d771302dcd3bd571c6797154a357c9f72
anna <annacarolinagarrido@gmail.com>
2022-11-17 17:00:03 -0300

    Initial styled components

--- projetogerminare/src/Pages/Home/index.jsx
+++ projetogerminare/src/Pages/Home/index.jsx
@@ -1,11 +1,50 @@
 import * as React from 'react';
 import { MainDiv } from './style';
+import HeaderImg from './Assets/HeaderImg.svg'
+import Tiktok from './Assets/Tiktok.svg'
+import Instagram from './Assets/Instagram.svg'
+import Facebook from './Assets/Facebook.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 export default function HomePage() {
     return (
         <>
             <MainDiv>
-                <div className="slider"> </div>
-                <div className="generalInfo"> </div>
+                <div className="slider">
+                    <div className="sliderContent">
+                        <h3 className="subtitle"> Germinatech </h3>
+                        <h1 className="maintitle"> Ensino médio de tecnologia </h1>
+                        <p className="description"> Ensino Médio Integrado com Técnico em Desenvolvimento de Sistemas </p>
+                        <div className="btnWhite"> Conheça mais </div>
+                    </div>
+                    <div className="mediaContainer">
+                        <div className="circleMedia">
+                                <img src={Tiktok} className="mediaIcon" />
+                        </div>
+                        <div className="circleMedia">
+                            <img src={Instagram} className="mediaIcon" />
+                        </div>
+                        <div className="circleMedia">
+                            <img src={Facebook} className="mediaIcon" />
+                        </div>
+                        </div>
+                </div>
+                <div className="generalInfo"> 
+                    <div>
+                        <div className='numberInfo'> 12 anos </div>
+                        <div className='dcpInfo'> fazendo história </div>
+                    </div>
+                    <div>
+                        <div className='numberInfo'> + 500 </div>
+                        <div className='dcpInfo'> alunos </div>
+                    </div>
+                    <div>
+                        <div className='numberInfo'> 6359m² </div>
+                        <div className='dcpInfo'> de estrutura </div>
+                    </div>
+                </div>
+                <FontAwesomeIcon icon={ faPlus } style={{width: '30px', height: '30px'}}/>
+                <img style={{ height: '20px', width: '20px' }} src={HeaderImg} />
             </MainDiv>
         </>
     )
--- projetogerminare/src/Pages/Home/style.jsx
+++ projetogerminare/src/Pages/Home/style.jsx
@@ -1,12 +1,87 @@
 import { styled } from "@mui/material";
 import Box from "@mui/material"
+import HeaderImg from './Assets/HeaderImg.svg'
 export const MainDiv = styled('div')({
+    fontFamily: 'Montserrat, sans-serif',
     '& .slider' : {
         backgroundColor: '#D2D2D2',
-        height: '603px'
+        backgroundImage: `linear-gradient(to right,rgb(25, 26, 75, 1), rgb(25, 26, 75, 0)), url(${HeaderImg})` ,
+        backgroundSize: 'cover',
+        height: '603px',
+        color: '#ffffff',
+        lineHeight: '55px',
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
+    '& .sliderContent' : {
+        display: 'flex',
+        flexDirection: 'column',
+        textAlign: 'left',
+        marginLeft: '130px',
+        paddingTop: '130px',
+        maxWidth: '31%',
+    },
+    '& .subtitle' : {
+        textTransform: 'uppercase',
+        fontSize: '20px',
+        fontWeight: '400',
+        letterSpacing: '7px',
+        margin: '0',
+    },
+    '& .maintitle' : {
+        marginTop: '26.6px',
+        fontSize: '55px',
+        marginBottom: '0',
+        lineHeight: '58px',
+    },
+    '& .description' : {
+        fontSize: '18px',
+        lineHeight: '26px',
+        fontWeight: '400',
+        marginBottom: '0px',
+    },
+    '& .btnWhite' : {
+        width: '220px',
+        textAlign: 'center',
+        marginTop: '43px',
+        padding: '0 10px',
+        textTransform: 'uppercase',
+        backgroundColor: '#ffffff',
+        fontSize: '20px',
+        border: 'none',
+        borderRadius: '12px',
+        color: '#331A42',
+        fontWeight: '700',
+        cursor: 'pointer',
+    },
+    '& .mediaContainer' : {
+        paddingTop: '193px',
+        marginRight: '130px',
+    },
+    '& .circleMedia' : {
+        height: '60px',
+        width: '60px',
+        color: '#2E3092',
+        borderRadius: '50%'
+    },
+    '& .mediaIcon' : {
+        marginTop: '12px'
+    },
+    '& .numberInfo' : {
+        fontSize: '60px',
+        fontWeight: '700',
+        color: 'linear-gradient(to right, rgba(255,0,0,0), rgba(255,0,0,1))'
+    },
+    '& .dcpInfo' : {
+        fontSize: '25px'
     },
     '& .generalInfo' : {
         backgroundColor: '#A8A8A8',
-        height: '247px'
-    }
+        height: '259px',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly'
+    },
+
 })
