b4f635abe0269add49a9ae58031e18446d3d15ec
Gabriela Saito <gabrielasaitop@gmail.com>
2022-11-24 23:10:35 -0300

    adding styled components lib, creating typography and button components

---
+++ projetogerminare/src/components/Button/Button.style.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  border-radius: 8px;
+  border: none;
+  padding: 10px 50px;
+  text-transform: uppercase;
+  font-weight: 700;
+  box-shadow: 0px 4px 4px 0px #00000040;
+`;
+
+export const BlueButton = styled(StyledButton)`
+  background-color: #191A4B;
+  color: white;
+  cursor: pointer;
+`;
+export const WhiteButton = styled(StyledButton)`
+  background-color: #FFFFFF;
+  color: #191A4B;
+  cursor: pointer;
+`;
+
+export const DisabledButton = styled(StyledButton)`
+  background-color: 'gray';
+  color: '#323232';
+`
